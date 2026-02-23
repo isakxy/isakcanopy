fd4fa9716abf009a51d8b16ab2b80d6c77922935
isakxy <isakxyzbase@gmail.com>
2026-02-23 19:46:47 +0700

    Add deployment script for token contract

---
+++ scripts/deployToken.ts
@@ -0,0 +1,17 @@
+// deployment script for the token contract
+
+const { ethers } = require('hardhat');
+
+async function main() {
+    const Token = await ethers.getContractFactory('Token');
+    const token = await Token.deploy();
+    await token.deployed();
+    console.log(`Token deployed to: ${token.address}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
